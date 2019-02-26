4d0d424d374bad8ee9a1e91b7f237d307fcc7ddb
Oscar Gustafsson <oscar.gustafsson2@medieinstitutet.se>
2019-02-26 10:58:23 +0100

    first commit

---
+++ src/views/AddTodo.vue
@@ -0,0 +1,26 @@
+<template>
+    <main id="addTodo">
+        <section>
+            <p>Dela denna lista via:</p>
+            <p>https://kungog.se/</p>
+        </section>
+        <section>
+            <input>
+        </section>
+        <section>
+            <button>Lägg till!</button>
+        </section>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'AddTodo',
+
+}
+</script>
+<style lang="scss">
+#addTodo {
+    background: yellow;
+}
+</style>
---
+++ src/views/Todo.vue
@@ -0,0 +1,30 @@
+<template>
+    <main id="todo">
+       <section class="main-todo">
+            <h1>Telefon</h1>
+            <h3>hjälp när det inte fastnar</h3>
+       </section>
+       <section>
+           <ul>
+               <li>Köpa</li>
+               <li>Lista</li>
+               <li>Makaroner</li>
+               <li>Bilar</li>
+           </ul>
+       </section>
+    </main>
+</template>
+
+<script>
+export default {
+    name: 'todo',
+    data () {
+        return {
+            todo: {
+                text: 'Bananer',
+                done: false
+            }
+        }
+    }
+}
+</script>
